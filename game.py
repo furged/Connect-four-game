a79b91a50c69dab261ad2c7095f51b325fb1439c
Anushka Shakya <chiyaa2005@gmail.com>
2025-07-05 19:17:46 +0530

    player 2 win function

--- game.py
+++ game.py
@@ -1,86 +1,82 @@
-import numpy as np # numerical computing library 
+import numpy as np 
 
 ROW_COUNT = 6
 COLUMN_COUNT = 7
 
 def create_board():
-    board = np.zeros((ROW_COUNT, COLUMN_COUNT))  # , dtype = int) can also be added if we want integer zeros [0 instead of 0.]
-    # zeros() is a method of np which creates a numpy array .. the shape is gonna be 6 rows and 7 columns. refer the output to see what it looks like 
+    board = np.zeros((ROW_COUNT, COLUMN_COUNT))  # , dtype = int) -> [0 instead of 0.]
     return board
 
-# define a function to actually drop a piece:
 def drop_piece(board, row, col, piece):
-    board[row][col] = piece # fill the board w whatever piece the player would drop
+    board[row][col] = piece 
 
-# this function is to check that if the location entered by thhe palyer is valid and its gonna check if the top row of that column is still emoty or not
 def is_valid_location(board, col):
-    return board[ROW_COUNT-1][col] == 0 # if this is true then we're good to let the player drop the piece in that column otherwise not
+    return board[ROW_COUNT-1][col] == 0 
     
-# to get the next open row in that column that we checked was valid/ to see what row the piece should go when the player selects the column
 def get_next_open_row(board, col):
-    for r in range(ROW_COUNT): # we're gonna check the board position (if some row is 0 then its empty and then its gonna return its first instance of it.. (we start checking from below))
+    for r in range(ROW_COUNT): 
         if board[r][col] == 0:
             return r 
 
 def print_board(board):
-    print(np.flip(board, 0))
+    print(np.flip(board, 0)) # 0 is for vertical flip and 1 is for horizontal flip and "None" is for both combined
     
 def winning_move(board, piece):
-    #gonna check horizontal patterns if any
-    for c in range (COLUMN_COUNT-3): # we subtract 3 cus after 4th column no pattern of 4 can be formed hiorizontally as we are checking from left to right
+    # Horizontal 
+    for c in range (COLUMN_COUNT-3):
         for r in range(ROW_COUNT):
             if board[r][c] == piece and board[r][c+1] == piece and  board[r][c+2] == piece and  board[r][c+3] == piece:
                 return True
             
-    for c in range (COLUMN_COUNT): # we subtract 3 cus after 4th row from bottom no pattern of 4 can be formed vertically
+    # Vertical
+    for c in range (COLUMN_COUNT):
         for r in range(ROW_COUNT -3):
             if board[r][c] == piece and board[r+1][c] == piece and  board[r+2][c] == piece and  board[r+3][c] == piece:
                 return True
             
-    #for positive slope diagonal  that will be ROW_COUNT-3 and COLUMN_COUNT-3
-    for c in range (COLUMN_COUNT-3): # we subtract 3 cus after 4th row from bottom no pattern of 4 can be formed vertically
+    # Positive slope diagonal
+    for c in range (COLUMN_COUNT-3): 
         for r in range(ROW_COUNT -3):
             if board[r][c] == piece and board[r+1][c+1] == piece and  board[r+2][c+2] == piece and  board[r+3][c+3] == piece:
                 return True
 
-    # for negative slope diagnonal
-    for c in range (COLUMN_COUNT-3): # we subtract 3 cus after 4th row from bottom no pattern of 4 can be formed vertically
+    # Negative slope diagnonal
+    for c in range (COLUMN_COUNT-3): 
         for r in range(3, ROW_COUNT):
             if board[r][c] == piece and board[r-1][c+1] == piece and  board[r-2][c+2] == piece and  board[r-3][c+3] == piece:
                 return True
 
-
-# to see what the board looks like:
 board = create_board()
 print_board(board)
 
 board = create_board()
-game_over = False # will only switch to true when someone gets 4 in a row
-turn = 0 # to track that which players turn is it
+game_over = False 
+turn = 0 
 
 while not game_over:
-    # ask for player 1 input
+    
     if turn == 0:
         col = int(input("Player 1 Make your Selection (0-6):"))   
-        #now we got the column, lets check that if location is valid:
-        if is_valid_location(board, col):
-            row = get_next_open_row(board, col)
+        row = get_next_open_row(board, col)
 
-            drop_piece(board, row, col, 1)
+        drop_piece(board, row, col, 1)
 
-            if winning_move(board, 1):
-                print("Player 1 Wins!")
-                game_over = True
+        if winning_move(board, 1):
+            print("Player 1 Wins!")
+            game_over = True
         
-
-    # ask for player 2 input
     else:
         col = int(input("Player 2 Make your Selection (0-6):")) 
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 2)
 
+        if winning_move(board, 2):
+            print("Player 2 Wins!")
+            game_over = True
+        
+
     print_board(board)
-    # and now no matter whos turn it was we're gonna have to increase turn by 1
+    
     turn += 1
-    turn = turn % 2 # this will make the turn alternate bw 0 and 1 so 0 would be player 1 turn and 1 would be player 2 turn
+    turn = turn % 2 
